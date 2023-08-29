a479837529e915282f75cfeda12346c393cec21c
Ritsu <enjoythepain1337@gmail.com>
2023-08-29 14:05:28 +0300

    tetst

---
+++ dev/plugin.spec.ts
@@ -0,0 +1,24 @@
+import type { Server } from 'http';
+import mongoose from 'mongoose';
+import payload from 'payload';
+import { start } from './src/server';
+
+describe('Plugin tests', () => {
+  let server: Server;
+
+  beforeAll(async () => {
+    server = await start({ local: true });
+  });
+
+  afterAll(async () => {
+    await mongoose.connection.dropDatabase();
+    await mongoose.connection.close();
+    server.close();
+  });
+
+  // Add tests to ensure that the plugin works as expected
+
+  // Example test to check for seeded data
+
+  expect(true).toBe(true);
+});
