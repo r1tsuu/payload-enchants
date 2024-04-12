3b9ba910e8024d58b26739c3cb03bfa2e0c8a858
Ritsu <enjoythepain1337@gmail.com>
2024-04-12 19:25:57 +0300

    delete unused

--- plugin/src/server/canFieldBeTranslated.ts
+++
@@ -1,6 +0,0 @@
-import type { Field } from 'payload/types';
-import { fieldAffectsData } from 'payload/types';
-
-export const canFieldBeTranslated = (field: Field) => {
-  if (!fieldAffectsData(field)) return false;
-};
