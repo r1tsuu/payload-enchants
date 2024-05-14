2d50bdd0c2cc686fb9172106b54cf3b019608f79
Ritsu <enjoythepain1337@gmail.com>
2024-05-14 11:55:51 +0300

    chore(translator): i18n

--- packages/translator/src/i18n-translations.ts
+++ packages/translator/src/i18n-translations.ts
@@ -23,4 +23,27 @@ export const translations = {
       resolver_openai_successMessage: 'Successfully translated. Press "Save" to apply the changes.',
     },
   },
+  ua: {
+    // Translations in Ukrainian
+    'plugin-translator': {
+      resolver_copy_buttonLabel: 'Копіювати з іншої локалі',
+      resolver_copy_errorMessage: 'Сталася помилка під час спроби перекладу даних',
+      resolver_copy_modalTitle: 'Виберіть локаль для копіювання',
+      resolver_copy_submitButtonLabelEmpty: 'Копіювати лише порожні поля',
+      resolver_copy_submitButtonLabelFull: 'Копіювати все',
+      resolver_copy_successMessage: 'Успішно скопійовано. Натисніть "Зберегти", щоб застосувати зміни.',
+      resolver_google_buttonLabel: 'Переклад Google',
+      resolver_google_errorMessage: 'Сталася помилка під час спроби перекладу даних',
+      resolver_google_modalTitle: 'Виберіть локаль для перекладу',
+      resolver_google_submitButtonLabelEmpty: 'Перекласти лише порожні поля',
+      resolver_google_submitButtonLabelFull: 'Перекласти все',
+      resolver_google_successMessage: 'Успішно перекладено. Натисніть "Зберегти", щоб застосувати зміни.',
+      resolver_openai_buttonLabel: 'Переклад AI',
+      resolver_openai_errorMessage: 'Сталася помилка під час спроби перекладу даних',
+      resolver_openai_modalTitle: 'Виберіть локаль для перекладу',
+      resolver_openai_submitButtonLabelEmpty: 'Перекласти лише порожні поля',
+      resolver_openai_submitButtonLabelFull: 'Перекласти все',
+      resolver_openai_successMessage: 'Успішно перекладено. Натисніть "Зберегти", щоб застосувати зміни.',
+    },
+  },
 };
